3200303aca54b7414e446187344ae508e91ec384
くもももん <92201232+kumomomon@users.noreply.github.com>
2024-06-09 23:18:15 +0800

    Update wapt_scan.sh
    Update to script to include command at the top of the txt file when they are created. Fix whois command as well.

--- wapt_scan.sh
+++ wapt_scan.sh
@@ -8,32 +8,66 @@ WHITE="\033[0;37m"
 
 if [ -z "$hostname" ]
 then
-	echo -e "${RED}Hostname is not provided${WHITE}"
-	echo "Usage: ./wapt_scan.sh <URL>"
-	echo "Example: ./wapt_scan.sh www.example.com"
+        echo -e "${RED}Hostname is not provided${WHITE}"
+        echo "Usage: ./wapt_scan.sh <URL>"
+        echo "Example: ./wapt_scan.sh www.example.com"
+	exit 1
 else
-	echo "Checking if $hostname directory exist"
-	if [ -d "$hostname" ]
-	then
-		echo "Directory exist!"
-	else echo "Creating directory!"
-		mkdir $hostname
-	fi
-	if [ -f "$hostname/nmapTCPscan.txt" ]
-	then
-		echo "Nmap TCP scan was already performed! Skipping Nmap TCP scan"
-	else echo -e "Running TCP Nmap scan on ${RED}$hostname${WHITE}"
-		nmap -T4 -p- -A $hostname > $hostname/nmapTCPscan.txt
-	fi
-	# if [ -f "$hostname/testssh.txt" ]
-	# then
-	#	echo "Nmap TCP scan was already performed! Skipping Nmap TCP scan"
-	#	nmap -T4 -p- -A $hostname > $hostname/nmapTCPscan.txt
-	# fi
-	if [ -f "$hostname/whatweb.txt" ]
-	then
-		echo "WhatWeb was already performed! Skipping WhatWeb scan"
-	else echo -e "Running WhatWeb on ${RED}$hostname${WHITE}"
-		whatweb https://$hostname > $hostname/whatweb.txt
-	fi
+        echo "Checking if $hostname directory exist"
+        if [ -d "$hostname" ]
+        then
+                echo "Directory exist!"
+        else echo "Creating directory!"
+                mkdir $hostname
+        fi
+        if [ -f "$hostname/nmapTCPscan.txt" ]
+        then
+                echo "Nmap TCP scan was already performed! Skipping Nmap TCP scan"
+        else echo -e "Running TCP Nmap scan on ${RED}$hostname${WHITE}"
+		echo -e "Command: nmap -T4 -p- -A $hostname\n" > $hostname/nmapTCPscan.txt
+                nmap -T4 -p- -A $hostname >> $hostname/nmapTCPscan.txt
+        fi
+        #if [ -f "$hostname/testssh.txt" ]
+        #then
+#               echo "testssh was already performed! Skipping testssh check"
+        #       .
+        #fi
+        if [ -f "$hostname/whatweb.txt" ]
+        then
+                echo "WhatWeb was already performed! Skipping WhatWeb scan"
+        else echo -e "Running WhatWeb on ${RED}$hostname${WHITE}"
+		echo -e "Command: whatweb https://$hostname\n" > $hostname/whatweb.txt
+                whatweb https://$hostname >> $hostname/whatweb.txt
+        fi
+        if [ -f "$hostname/whois.txt" ]
+        then
+                echo "WhoIS was already performed! Skipping WhoIS search"
+        else echo -e "Running WhoIS on ${RED}$hostname${WHITE}"
+		wwwless_hostname=$(echo "$hostname" | sed 's/^www\.//')
+  		echo -e "Command: whois $wwwless_hostname\n" > $hostname/whois.txt
+                whois $wwwless_hostname >> $hostname/whois.txt
+        fi
+        if [ -f "$hostname/wafw00f.txt" ]
+        then
+                echo "Wafw00f was already performed! Skipping Wafw00f check"
+        else echo -e "Running Wafw00f on ${RED}$hostname${WHITE}"
+		echo -e "Command: wafw00f https://$hostname\n" > $hostname/wafw00fs.txt
+                wafw00f https://$hostname >> $hostname/wafw00fs.txt
+        fi
+        if [ -f "$hostname/lbd.txt" ]
+        then
+                echo "LBD was already performed! Skipping LBD load balancer check"
+        else echo -e "Running LBD on ${RED}$hostname${WHITE}"
+		echo -e "Command: lbd $hostname\n" > $hostname/lbd.txt
+                lbd $hostname >> $hostname/lbd.txt
+        fi
+        if [ -f "$hostname/nikto.txt" ]
+        then
+                echo "Nikto was already performed! Skipping Nikto scan"
+        else echo -e "Running Nikto on ${RED}$hostname${WHITE}"
+		echo -e "Command: nikto -h https://$hostname" > $hostname/nikto.txt
+                nikto -h https://$hostname >> $hostname/nikto.txt
+        fi
+
 fi
+
